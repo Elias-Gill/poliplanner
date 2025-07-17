d5388b65c9c4862ec300cc90659022a65e52207b
Elias Gill <eliasgill42@gmail.com>
2025-07-17 20:58:44 -0300

    feat: mensaje para mostrar la herramienta de migracion

--- src/main/java/com/elias_gill/poliplanner/controller/DashboardController.java
+++ src/main/java/com/elias_gill/poliplanner/controller/DashboardController.java
@@ -14,7 +14,6 @@ import com.elias_gill.poliplanner.services.ScheduleService;
 import com.elias_gill.poliplanner.services.SheetVersionService;
 
 import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 
