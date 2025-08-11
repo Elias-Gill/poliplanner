be40b60cc481e28f34c8fdc648269a73d67b547f
Elias Gill <eliasgill42@gmail.com>
2025-08-11 12:01:08 -0300

    wip: funcion general del parser completa

--- excel/src/main/java/poliplanner/excel/parser/ExcelParser.java
+++ excel/src/main/java/poliplanner/excel/parser/ExcelParser.java
@@ -8,6 +8,7 @@ import java.io.InputStream;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -48,9 +49,10 @@ public class ExcelParser {
         try (InputStream is = new FileInputStream(new File(file.toUri()));
                 ReadableWorkbook wb = new ReadableWorkbook(is)) {
 
-            for (Sheet s : wb.getSheets().toList()) {
-                // Ignorar hojas basura
-                String career = s.getName();
+            Map<String, List<SubjectCsvDTO>> result = new HashMap<>();
+            for (Sheet sheet : wb.getSheets().toList()) {
+                // Ignorar hojas "basura"
+                String career = sheet.getName();
                 if (career.contains("odigos")
                         || career.contains("ódigos")
                         || career.contains("Asignaturas")
@@ -60,12 +62,26 @@ public class ExcelParser {
                     continue;
                 }
 
-                Row headerRow = searchHeadersRow(s);
+                List<Row> sheetRows = sheet.read();
+
+                Row headerRow = searchHeadersRow(sheetRows);
+                // Este metodo es 1 based, asi que ahora esta apuntando a la fila inmediatamente
+                // debajo de los encabezados.
+                Integer startingRow = headerRow.getRowNum();
+
                 Layout layout = findFittingLayout(headerRow);
 
-                // TODO: parsear materias el layout encontrado
+                List<SubjectCsvDTO> subjects = new ArrayList<>();
+                for (Integer i = startingRow; i < sheetRows.size(); i++) {
+                    Row row = sheetRows.get(i);
+                    SubjectCsvDTO subject = parseRow(row, layout);
+                    subjects.add(subject);
+                }
+
+                result.put(career, subjects);
             }
 
+            return result;
         } catch (Exception e) {
         }
         return null;
@@ -75,14 +91,13 @@ public class ExcelParser {
     // ======== Private methods ============
     // =====================================
 
-    // Debe retornar la columna de inicio del headers row (item)
-    private static Integer parseHeadersRow(Row row) {
-        // TODO: continuar
-        return 1;
+    private static SubjectCsvDTO parseRow(Row row, Layout layout) {
+        
+        return null;
     }
 
-    private static Row searchHeadersRow(Sheet sheet) throws FileNotFoundException, IOException {
-        for (Row r : sheet.read()) {
+    private static Row searchHeadersRow(List<Row> rows) throws FileNotFoundException, IOException {
+        for (Row r : rows) {
             if (r.getCellCount() < 0 || !isHeader(r)) {
                 continue;
             }
@@ -90,24 +105,6 @@ public class ExcelParser {
         }
 
         return null;
-    }
-
-    // Determina la fila del Excel de encabezados buscando la celda que
-    // contenga el texto "ítem" o "item" (case insensitive).
-    private static boolean isHeader(Row r) {
-        // Implementación con Stream que:
-        // 1. Filtra solo celdas de tipo STRING (not null)
-        // 2. Normaliza el contenido (minúsculas y sin espacios)
-        // 3. Early return al encontrar una coincidencia
-        return r.stream()
-                .filter(cell -> (cell != null && cell.getType() == CellType.STRING))
-                .map(cell -> ((String) cell.getValue()).toLowerCase().trim())
-                .anyMatch(value -> value.contains("ítem") || value.contains("item"));
-    }
-
-    private static boolean isEmptyRow(Row r) {
-        return r.stream().allMatch(
-                cell -> cell == null || cell.getValue() == null || cell.getValue().toString().trim().isEmpty());
     }
 
     /**
@@ -147,6 +144,10 @@ public class ExcelParser {
         throw new IllegalArgumentException("No matching layout found for row: " + r);
     }
 
+    // =====================================
+    // ======== Utility methods ============
+    // =====================================
+
     private String getStringCellValueSafe(List<Cell> cells, int index) {
         if (index >= cells.size() || cells.get(index) == null) {
             return "";
@@ -154,7 +155,21 @@ public class ExcelParser {
         return cells.get(index).getText();
     }
 
-    // =====================================
-    // ======== Utility methods ============
-    // =====================================
+    // Determina la fila del Excel de encabezados buscando la celda que
+    // contenga el texto "ítem" o "item" (case insensitive).
+    private static boolean isHeader(Row r) {
+        // Implementación con Stream que:
+        // 1. Filtra solo celdas de tipo STRING (not null)
+        // 2. Normaliza el contenido (minúsculas y sin espacios)
+        // 3. Early return al encontrar una coincidencia
+        return r.stream()
+                .filter(cell -> (cell != null && cell.getType() == CellType.STRING))
+                .map(cell -> ((String) cell.getValue()).toLowerCase().trim())
+                .anyMatch(value -> value.contains("ítem") || value.contains("item"));
+    }
+
+    private static boolean isEmptyRow(Row r) {
+        return r.stream().allMatch(
+                cell -> cell == null || cell.getValue() == null || cell.getValue().toString().trim().isEmpty());
+    }
 }
