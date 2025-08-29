7d9d9ff1370c1048820911588f15e4a369bb7830
Elias Gill <eliasgill42@gmail.com>
2025-08-29 13:00:29 -0300

    fix: errores de redireccion, tests y demas

--- web/src/main/java/poliplanner/controller/UserController.java
+++ web/src/main/java/poliplanner/controller/UserController.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -15,7 +14,6 @@ import poliplanner.services.exception.EmailAlreadyInUseException;
 import poliplanner.services.exception.InternalServerErrorException;
 import poliplanner.services.exception.UserNameAlreadyExistsException;
 import poliplanner.models.User;
-import poliplanner.services.EmailService;
 import poliplanner.services.UserService;
 
 import lombok.RequiredArgsConstructor;
